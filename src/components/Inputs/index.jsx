c122d49e0ca7d2fd7371f3366bb6593f6d9f3366
CaueRafael <caue_rrds@hotmail.com>
2022-04-21 14:53:25 -0300

    feat: componente input

---
+++ src/components/Inputs/index.jsx
@@ -0,0 +1,13 @@
+import { Container, InputContainer } from "./styles";
+
+const Input = ({ label, icon, ...rest }) => {
+  return (
+    <Container>
+      <div>{label}</div>
+      <InputContainer>
+        <input {...rest} />
+      </InputContainer>
+    </Container>
+  );
+};
+export { Input };
---
+++ src/components/Inputs/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  text-align: left;
+`;
+
+export const InputContainer = styled.div`
+  background: var(--grey3);
+  border-radius: 4px;
+  border: 2px solid var(--grey3);
+  color: var(--grey5);
+  padding: 1rem;
+  width: 100%;
+  display: flex;
+  /* transition: ; */
+  &:focus-within {
+    border: 2px solid var(--grey5);
+  }
+  input {
+    background: transparent;
+    align-items: center;
+    flex: 1;
+    border: 0;
+    color: var(--white);
+    &::placeholder {
+      color: var(--grey4);
+    }
+  }
+`;
